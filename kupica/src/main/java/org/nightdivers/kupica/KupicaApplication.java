05e5c9327929e6e810fbd0ecdef2e1fc2aa95262
unagi-zoso <unagi.zoso@gmail.com>
2024-04-06 23:25:05 +0900

    #13 Feat Thymeleaf config 클래스 추가 (decoupled logic 설정)
    `decoupled logic` 을 사용하기 위한 설정 클래스를 추가하였다.
    config 패키지에 생성된 `ThymeleafConfig` 를 빈으로 등록하기 위해 `@SpringBootApplication` 이 있는 클래스에게 `@ConfigurationPropertiesScan` 을 추가했다.

--- kupica/src/main/java/org/nightdivers/kupica/KupicaApplication.java
+++ kupica/src/main/java/org/nightdivers/kupica/KupicaApplication.java
@@ -2,7 +2,9 @@ package org.nightdivers.kupica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class KupicaApplication {
 

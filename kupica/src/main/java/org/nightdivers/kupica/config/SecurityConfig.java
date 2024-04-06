659fa476ec181efb18c9933761724b2e4ada8668
unagi-zoso <unagi.zoso@gmail.com>
2024-04-07 00:29:14 +0900

    #16 Feat 모든 요청 허용 spring security config 클래스 추가
    초기 개발 동안 spring security 허가 관련된 불편을 막기 위해 모든 요청에 대해 허용하는 spring security config 클래스를 추가하였다.

---
+++ kupica/src/main/java/org/nightdivers/kupica/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.nightdivers.kupica.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults())
+                .logout(withDefaults())
+                .build();
+    }
+}
+

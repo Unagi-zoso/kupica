ac6d6246ada5722b62982237c63517e747b54007
unagi-zoso <unagi.zoso@gmail.com>
2024-04-09 22:53:54 +0900

    #12 Refactor 업로드폼 표현 js 의 var -> const 변경, click event 부여방식 변경
    var 였던 변수들이  const 여도 문제 없어 변경하였다.
    
    클릭 시 이벤트를 부여하는 방식을 다른 버튼들과 동일하게 그 html 태그에 onclick 으로 부여하였다.

--- kupica/src/main/resources/static/js/ModalFormBtn.js
+++ kupica/src/main/resources/static/js/ModalFormBtn.js
@@ -1,18 +1,17 @@
-var modal = document.getElementById("upload__modal__form");
-var btn = document.getElementById("upload__btn");
-var span = document.getElementById("upload__modal__form__quit__btn");
+const uploadModal = document.getElementById("upload__modal__form");
+const span = document.getElementById("upload__modal__form__quit__btn");
 
-btn.onclick = function() {
-    modal.style.display = "block";
+function presentUploadModal() {
+    uploadModal.style.display = "block";
 }
 
 span.onclick = function() {
-    modal.style.display = "none";
+    uploadModal.style.display = "none";
 }
 
 window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
+    if (event.target === uploadModal) {
+        uploadModal.style.display = "none";
     }
 }
 

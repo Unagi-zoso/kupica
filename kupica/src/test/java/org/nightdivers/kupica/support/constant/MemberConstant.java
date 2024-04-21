a38c0c53851855a8f94ebee347789e813f3b39a9
unagi-zoso <unagi.zoso@gmail.com>
2024-04-21 23:56:47 +0900

    #29 Feat Member Entity 테스트 용 상수 및 데이터 생성 함수 구현
    MemberConstant 에서 **유효하지 않은 경우**의 MEMBER_ID 만 존재하는데 이는 유효한 경우의 ID 는 영속화를 거쳐야만 얻을 수 있고 애초에 **data.sql** 에서 정의한 테스트 데이터가 있어 임의로 ID 를 배정하기가 어려워서이다.

---
+++ kupica/src/test/java/org/nightdivers/kupica/support/constant/MemberConstant.java
@@ -0,0 +1,30 @@
+package org.nightdivers.kupica.support.constant;
+
+import org.nightdivers.kupica.domain.member.SocialLoginType;
+import org.nightdivers.kupica.domain.member.UserRole;
+
+public class MemberConstant {
+    public static final String TEST_MEMBER_1_NICKNAME = "test 1 nickname";
+    public static final String TEST_MEMBER_1_EMAIL = "test1@test.com";
+    public static final UserRole TEST_MEMBER_1_ROLE = UserRole.USER;
+    public static final SocialLoginType TEST_MEMBER_1_SOCIAL_LOGIN_TYPE = SocialLoginType.KAKAO;
+
+    public static final String TEST_MEMBER_2_NICKNAME = "test 2 nickname";
+    public static final String TEST_MEMBER_2_EMAIL = "test2@test.com";
+    public static final UserRole TEST_MEMBER_2_ROLE = UserRole.USER;
+    public static final SocialLoginType TEST_MEMBER_2_SOCIAL_LOGIN_TYPE = SocialLoginType.KAKAO;
+
+    public static final String TEST_MEMBER_3_NICKNAME = "test 3 nickname";
+    public static final String TEST_MEMBER_3_EMAIL = "test3@test.com";
+    public static final UserRole TEST_MEMBER_3_ROLE = UserRole.USER;
+    public static final SocialLoginType TEST_MEMBER_3_SOCIAL_LOGIN_TYPE = SocialLoginType.KAKAO;
+
+    public static final String TEST_MEMBER_4_NICKNAME = "test 4 nickname";
+    public static final String TEST_MEMBER_4_EMAIL = "test4@test.com";
+    public static final UserRole TEST_MEMBER_4_ROLE = UserRole.USER;
+    public static final SocialLoginType TEST_MEMBER_4_SOCIAL_LOGIN_TYPE = SocialLoginType.KAKAO;
+
+    public static final Long TEST_INVALID_MEMBER_ID = -1L;
+    public static final String TEST_INVALID_MEMBER_NICKNAME = "invalid nickname";
+    public static final String TEST_INVALID_MEMBER_EMAIL = "invalid@test.com";
+}

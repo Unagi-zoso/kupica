86c9a85d9da3fdd6f7182930800e09dabfa14d16
unagi-zoso <unagi.zoso@gmail.com>
2024-04-25 23:32:06 +0900

    #45 Refactor 클래스명 변경 Generator -> Factory, 닫는 괄호 줄넘김 추가

--- kupica/src/test/java/org/nightdivers/kupica/support/factory/MemberFactory.java
+++ kupica/src/test/java/org/nightdivers/kupica/support/factory/MemberFactory.java
@@ -1,4 +1,4 @@
-package org.nightdivers.kupica.support.generator;
+package org.nightdivers.kupica.support.factory;
 
 import static org.nightdivers.kupica.support.constant.MemberConstant.TEST_MEMBER_1_EMAIL;
 import static org.nightdivers.kupica.support.constant.MemberConstant.TEST_MEMBER_1_NICKNAME;
@@ -18,14 +18,17 @@ import static org.nightdivers.kupica.support.constant.MemberConstant.TEST_MEMBER
 import static org.nightdivers.kupica.support.constant.MemberConstant.TEST_MEMBER_4_SOCIAL_LOGIN_TYPE;
 
 import org.nightdivers.kupica.domain.member.Member;
+import org.nightdivers.kupica.domain.member.SocialLoginType;
+import org.nightdivers.kupica.domain.member.UserRole;
 
-public class MemberGenerator {
+public class MemberFactory {
     public static Member createTestMember1() {
         return Member.of(
                 TEST_MEMBER_1_NICKNAME,
                 TEST_MEMBER_1_EMAIL,
                 TEST_MEMBER_1_ROLE,
-                TEST_MEMBER_1_SOCIAL_LOGIN_TYPE);
+                TEST_MEMBER_1_SOCIAL_LOGIN_TYPE
+        );
     }
 
     public static Member createTestMember2() {
@@ -33,7 +36,8 @@ public class MemberGenerator {
                 TEST_MEMBER_2_NICKNAME,
                 TEST_MEMBER_2_EMAIL,
                 TEST_MEMBER_2_ROLE,
-                TEST_MEMBER_2_SOCIAL_LOGIN_TYPE);
+                TEST_MEMBER_2_SOCIAL_LOGIN_TYPE
+        );
     }
 
     public static Member createTestMember3() {
@@ -41,7 +45,8 @@ public class MemberGenerator {
                 TEST_MEMBER_3_NICKNAME,
                 TEST_MEMBER_3_EMAIL,
                 TEST_MEMBER_3_ROLE,
-                TEST_MEMBER_3_SOCIAL_LOGIN_TYPE);
+                TEST_MEMBER_3_SOCIAL_LOGIN_TYPE
+        );
     }
 
     public static Member createTestMember4() {
@@ -49,6 +54,16 @@ public class MemberGenerator {
                 TEST_MEMBER_4_NICKNAME,
                 TEST_MEMBER_4_EMAIL,
                 TEST_MEMBER_4_ROLE,
-                TEST_MEMBER_4_SOCIAL_LOGIN_TYPE);
+                TEST_MEMBER_4_SOCIAL_LOGIN_TYPE
+        );
+    }
+
+    public static Member createCustomMember(String nickname, String email) {
+        return Member.of(
+                nickname,
+                email,
+                UserRole.MEMBER,
+                SocialLoginType.KAKAO
+        );
     }
 }

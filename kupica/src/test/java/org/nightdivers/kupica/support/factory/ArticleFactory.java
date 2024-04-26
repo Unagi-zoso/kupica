7ce73f2b307340ab4e4bcad0bcf170c890a80416
unagi-zoso <unagi.zoso@gmail.com>
2024-04-27 00:05:25 +0900

    #47 Test ArticleRepository 테스트 관련 상수, 팩토리 메소드 정의
    `memberArticle`, `anonymousUserArticle` 이 구분되며 기본적인 생성함수에는 `member`, `anonymousUser` 가 `null` 로 두었다.
    
    테스트를 위해 `member`, `anonymousUser` 가 필요하다면 `createCustom...` 메소드를 사용하면 된다.

---
+++ kupica/src/test/java/org/nightdivers/kupica/support/factory/ArticleFactory.java
@@ -0,0 +1,78 @@
+package org.nightdivers.kupica.support.factory;
+
+import org.nightdivers.kupica.domain.anonymoususer.AnonymousUser;
+import org.nightdivers.kupica.domain.article.Article;
+import org.nightdivers.kupica.domain.member.Member;
+import org.nightdivers.kupica.support.constant.ArticleConstant;
+
+public class ArticleFactory {
+    public static Article createTestMemberArticle1() {
+        return Article.createMemberArticle(
+                ArticleConstant.TEST_MEMBER_ARTICLE_1_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestMemberArticle2() {
+        return Article.createMemberArticle(
+                ArticleConstant.TEST_MEMBER_ARTICLE_2_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestMemberArticle3() {
+        return Article.createMemberArticle(
+                ArticleConstant.TEST_MEMBER_ARTICLE_3_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestMemberArticle4() {
+        return Article.createMemberArticle(
+                ArticleConstant.TEST_MEMBER_ARTICLE_4_CAPTION,
+                null
+        );
+    }
+
+    public static Article createCustomMemberArticle(String caption, Member member) {
+        return Article.createMemberArticle(
+                caption,
+                member
+        );
+    }
+
+    public static Article createTestAnonymousArticle1() {
+        return Article.createAnonymousArticle(
+                ArticleConstant.TEST_ANONYMOUS_ARTICLE_1_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestAnonymousArticle2() {
+        return Article.createAnonymousArticle(
+                ArticleConstant.TEST_ANONYMOUS_ARTICLE_2_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestAnonymousArticle3() {
+        return Article.createAnonymousArticle(
+                ArticleConstant.TEST_ANONYMOUS_ARTICLE_3_CAPTION,
+                null
+        );
+    }
+
+    public static Article createTestAnonymousArticle4() {
+        return Article.createAnonymousArticle(
+                ArticleConstant.TEST_ANONYMOUS_ARTICLE_4_CAPTION,
+                null
+        );
+    }
+
+    public static Article createCustomAnonymousArticle(String caption, AnonymousUser anonymousUser) {
+        return Article.createAnonymousArticle(
+                caption,
+                anonymousUser
+        );
+    }
+}

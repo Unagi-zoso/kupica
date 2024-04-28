c602936f21b9fb8a4a05c9d27e1fb7d0298b37bb
unagi-zoso <unagi.zoso@gmail.com>
2024-04-29 00:02:09 +0900

    #47 Feat CommentRepository 및 테스트 코드 작성
    `JpaRepository` 를 통해 생성하였다.
    
    `DB` , `Repository` 단에서는 댓글과 대댓글을 모두 같은 `Comment` Entity 로 다루며 이가 구분되서 반환되는 건 Service 단에 들어가서 처리된다.

---
+++ kupica/src/main/java/org/nightdivers/kupica/domain/comment/CommentRepository.java
@@ -0,0 +1,20 @@
+package org.nightdivers.kupica.domain.comment;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByArticleId(Long id);
+
+    List<Comment> findAllByMemberId(Long id);
+
+    List<Comment> findAllByMemberNickname(String nickname);
+
+    List<Comment> findAllByArticleIdAndMemberId(Long articleId, Long memberId);
+
+    List<Comment> findAllByArticleIdAndMemberNickname(Long id, String nickname);
+
+    List<Comment> findAllByAnonymousUserNickname(String nickname);
+
+    List<Comment> findAllByArticleIdAndAnonymousUserNickname(Long id, String nickname);
+}

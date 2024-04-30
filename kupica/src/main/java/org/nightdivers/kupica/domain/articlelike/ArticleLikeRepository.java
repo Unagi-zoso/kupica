844ca155ccb3d8bbacbad518783af45f04b70777
unagi-zoso <unagi.zoso@gmail.com>
2024-04-30 19:18:33 +0900

    #47 Feat 좋아요 수를 기준으로 `Article` 을 조회하는 쿼리 메소드 추가
    `JPQL`을 통해 만들었다. 기존엔 `ArticleRepository ` 에서 구현하였지만 3 개의 테이블을 조인해야했고 외래키를 가지고 있지 `ArticleLike` 에서 이를 구현하는게 더 자연스럽다고 생각했다.

--- kupica/src/main/java/org/nightdivers/kupica/domain/articlelike/ArticleLikeRepository.java
+++ kupica/src/main/java/org/nightdivers/kupica/domain/articlelike/ArticleLikeRepository.java
@@ -1,10 +1,35 @@
 package org.nightdivers.kupica.domain.articlelike;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
     List<ArticleLike> findAllByMemberId(Long memberId);
 
     List<ArticleLike> findAllByArticleId(Long articleId);
+
+    Long countByArticleId(Long articleId);
+
+    @Query( "SELECT al " +
+            "FROM ArticleLike al " +
+            "WHERE al.id IN ( " +
+                "SELECT MIN(al2.id) " +
+                "FROM ArticleLike al2 " +
+                "GROUP BY al2.article.id " +
+                "ORDER BY COUNT(al2.article.id) DESC)"
+    )
+    Page<ArticleLike> findArticleIdOrderByCountDesc(Pageable pageable);
+
+    @Query( "SELECT al " +
+            "FROM ArticleLike al " +
+            "WHERE al.id IN ( " +
+                "SELECT MIN(al2.id) " +
+                "FROM ArticleLike al2 " +
+                "GROUP BY al2.article.id " +
+                "ORDER BY COUNT(al2.article.id) ASC)"
+    )
+    Page<ArticleLike> findArticleIdOrderByCountAsc(Pageable pageable);
 }
--- kupica/src/test/java/org/nightdivers/kupica/domain/articlelike/ArticleLikeRepositoryTest.java
+++ kupica/src/test/java/org/nightdivers/kupica/domain/articlelike/ArticleLikeRepositoryTest.java
@@ -1,8 +1,10 @@
 package org.nightdivers.kupica.domain.articlelike;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.nightdivers.kupica.domain.articlelike.ArticleLike.createAnonymousArticleLike;
 import static org.nightdivers.kupica.domain.articlelike.ArticleLike.createMemberArticleLike;
+import static org.nightdivers.kupica.support.constant.AnonymousUserConstant.TEST_ANONYMOUS_IP_LIST;
 import static org.nightdivers.kupica.support.constant.ArticleConstant.TEST_INVALID_ARTICLE_ID;
 import static org.nightdivers.kupica.support.constant.MemberConstant.TEST_INVALID_MEMBER_ID;
 import static org.nightdivers.kupica.support.factory.AnonymousUserFactory.createTestAnonymousUser1;
@@ -27,6 +29,7 @@ import org.nightdivers.kupica.domain.article.ArticleRepository;
 import org.nightdivers.kupica.domain.member.Member;
 import org.nightdivers.kupica.domain.member.MemberRepository;
 import org.nightdivers.kupica.support.annotation.RepositoryTest;
+import org.springframework.data.domain.PageRequest;
 
 @RequiredArgsConstructor
 @RepositoryTest
@@ -44,6 +47,7 @@ class ArticleLikeRepositoryTest {
 
     Article givenArticle1;
     Article givenArticle2;
+    Article givenArticle3;
 
     ArticleLike givenArticleLike1;
     ArticleLike givenArticleLike2;
@@ -62,6 +66,7 @@ class ArticleLikeRepositoryTest {
 
         givenArticle1 = articleRepository.save(createTestMemberArticle1());
         givenArticle2 = articleRepository.save(createTestMemberArticle2());
+        givenArticle3 = articleRepository.save(createTestMemberArticle3());
 
         givenArticleLike1 = articleLikeRepository.save(createMemberArticleLike(givenMember1, givenArticle1));
         givenArticleLike2 = articleLikeRepository.save(createMemberArticleLike(givenMember1, givenArticle2));
@@ -146,6 +151,54 @@ class ArticleLikeRepositoryTest {
         assertThat(articleLikeList).isEmpty();
     }
 
+    @DisplayName("좋아요 수가 많은 순서로 조회 - [성공]")
+    @Test
+    void whenFindArticleIdOrderByCountDesc_thenReturnsArticleLikeList() {
+        // given
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size())
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle1)));
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size() - 1)
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle2)));
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size() - 2)
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle3)));
+
+        // when
+        List<ArticleLike> actualArticleLikes = articleLikeRepository.findArticleIdOrderByCountDesc(
+                PageRequest.of(0, 10))
+                .getContent();
+        // then
+        assertAll(
+                () -> assertThat(actualArticleLikes.getFirst().getArticle().getId()).isEqualTo(givenArticle1.getId()),
+                () -> assertThat(actualArticleLikes.get(1).getArticle().getId()).isEqualTo(givenArticle2.getId()),
+                () -> assertThat(actualArticleLikes.get(2).getArticle().getId()).isEqualTo(givenArticle3.getId())
+        );
+    }
+
+    @DisplayName("좋아요 수가 적은 순서로 조회 - [성공]")
+    @Test
+    void whenFindArticleIdOrderByCountAsc_thenReturnsArticleLikeList() {
+        // given
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size())
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle1)));
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size()-1)
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle2)));
+        TEST_ANONYMOUS_IP_LIST.subList(0, TEST_ANONYMOUS_IP_LIST.size()-2)
+                .forEach(ip -> articleLikeRepository.save(createAnonymousArticleLike(ip, givenArticle3)));
+
+
+        // when
+        List<ArticleLike> actualArticleLikes = articleLikeRepository.findArticleIdOrderByCountAsc(
+                PageRequest.of(0, 10))
+                .getContent();
+
+        // then
+        assertAll(
+                () -> assertThat(actualArticleLikes.getLast().getArticle().getId()).isEqualTo(givenArticle1.getId()),
+                () -> assertThat(actualArticleLikes.get(actualArticleLikes.size()-2).getArticle().getId()).isEqualTo(givenArticle2.getId()),
+                () -> assertThat(actualArticleLikes.get(actualArticleLikes.size()-3).getArticle().getId()).isEqualTo(givenArticle3.getId())
+        );
+    }
+
 
     /* 좋아요 생성 */
     @DisplayName("member 좋아요 생성 - [성공]")
@@ -187,5 +240,31 @@ class ArticleLikeRepositoryTest {
 
         // then
         assertThat(articleLikeRepository.findById(articleLike.getId())).isEmpty();
+    }
+
+
+    /* 좋아요 수 조회 */
+    @DisplayName("article id 와 일치하는 좋아요 수 조회 - [성공]")
+    @Test
+    void givenArticleId_whenCountByArticleId_thenReturnsCount() {
+        // given
+
+        // when
+        Long count = articleLikeRepository.countByArticleId(givenArticle1.getId());
+
+        // then
+        assertThat(count).isEqualTo(3);
+    }
+
+    @DisplayName("article id 와 일치하는 좋아요 수 조회 - [실패 : 유효하지 않은 article id]")
+    @Test
+    void givenInvalidArticleId_whenCountByArticleId_thenReturnsZero() {
+        // given
+
+        // when
+        Long count = articleLikeRepository.countByArticleId(TEST_INVALID_ARTICLE_ID);
+
+        // then
+        assertThat(count).isZero();
     }
 }

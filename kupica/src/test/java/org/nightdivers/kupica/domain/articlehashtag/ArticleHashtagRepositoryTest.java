d3be6bc2ca5754fb7cfefd5017e4878197c797df
unagi-zoso <unagi.zoso@gmail.com>
2024-04-30 19:30:30 +0900

    #47 Feat ArticleHashtagRepository 및 테스트 코드 작성
    `JpaRepository` 를 통해 구현하였다.

---
+++ kupica/src/test/java/org/nightdivers/kupica/domain/articlehashtag/ArticleHashtagRepositoryTest.java
@@ -0,0 +1,45 @@
+package org.nightdivers.kupica.domain.articlehashtag;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.nightdivers.kupica.support.constant.HashtagConstant.TEST_INVALID_TAG_NAME;
+import static org.nightdivers.kupica.support.constant.HashtagConstant.TEST_VALID_TAG_NAME;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.nightdivers.kupica.support.annotation.RepositoryTest;
+import org.springframework.data.domain.PageRequest;
+
+@RequiredArgsConstructor
+@RepositoryTest
+class ArticleHashtagRepositoryTest {
+    private final ArticleHashtagRepository articleHashtagRepository;
+
+    /* Article Hashtag 조회 */
+    @DisplayName("Hashtag 와 일치하는 Article 페이지로 조회 - [성공]")
+    @Test
+    void givenHashtag_whenFindArticleByHashtag_thenReturnArticlePage() {
+        // given
+
+
+        // when
+        List<ArticleHashtag> articleHashtags = articleHashtagRepository.findByHashtagTagName(PageRequest.of(0, 5), TEST_VALID_TAG_NAME).getContent();
+
+        // then
+        assertThat(articleHashtags).isNotEmpty();
+    }
+
+    @DisplayName("Hashtag 와 일치하는 Article 페이지로 조회 - [실패 : Hashtag 미존재]")
+    @Test
+    void givenHashtag_whenFindArticleByHashtag_thenEmpty() {
+        // given
+
+        // when
+        List<ArticleHashtag> articleHashtags = articleHashtagRepository.findByHashtagTagName(PageRequest.of(0, 5),
+                TEST_INVALID_TAG_NAME).getContent();
+
+        // then
+        assertThat(articleHashtags).isEmpty();
+    }
+}

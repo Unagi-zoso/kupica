663a08dd3899f1378b16504cd6d3a277ae25bdc1
corvo789 <corvo789@naver.com>
2024-05-02 21:18:01 +0900

    #52 Test OracleDB 도입으로 인한 테스트 데이터 변경
    SEQUENCE, VARCAHR2, NUMERIC 등 Oracle 문법에 호환하게 끔 구성하였다.
    
    테스트 환경에서 시퀀스가 기존에 있는 테스트 데이터를 인지하지 못해 테스트 데이터가 있음에도 1 부터 시작한다. 그런 점을 막기 위해 시퀀스 생성 시 테스트 데이터 수 다음부터 시퀀스가 부여되게 초기화 하였다.
    
    이후 유지보수하다 이부분을 놓치고 문제가 발생하지 않을까 고민이 된다.

--- kupica/src/test/resources/sql/schema-h2.sql
+++ kupica/src/test/resources/sql/schema-h2.sql
@@ -1,98 +1,123 @@
-CREATE TABLE IF NOT EXISTS member (
-                                      id BIGINT NOT NULL AUTO_INCREMENT,
-                                      nickname VARCHAR(18) NOT NULL,
-                                      email_address VARCHAR(255) NOT NULL,
-                                      user_role VARCHAR(255) NOT NULL,
-                                      social_login_type VARCHAR(255) NOT NULL,
-                                      created_datetime DATETIME(6) NOT NULL,
-                                      updated_datetime DATETIME(6) NOT NULL,
-                                      primary key (id)
-) engine=InnoDB;
+CREATE TABLE member
+(
+    id                NUMERIC(19, 0) NOT NULL,
+    nickname          VARCHAR2(18)   NOT NULL UNIQUE,
+    email_address     VARCHAR2(255)  NOT NULL UNIQUE,
+    user_role         VARCHAR2(255)  NOT NULL CHECK (user_role IN ('ADMIN', 'MEMBER', 'ANONYMOUS')),
+    social_login_type VARCHAR2(20)   NOT NULL CHECK (social_login_type IN ('KAKAO')),
+    created_datetime  TIMESTAMP(6)   NOT NULL,
+    updated_datetime  TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS anonymous_user (
-                                id BIGINT NOT NULL AUTO_INCREMENT,
-                                nickname VARCHAR(18) NOT NULL,
-                                password VARCHAR(64) NOT NULL,
-                                ip_address VARCHAR(24) NOT NULL,
-                                user_role VARCHAR(255) NOT NULL,
-                                created_datetime DATETIME(6) NOT NULL,
-                                primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS article (
-                                       id BIGINT NOT NULL AUTO_INCREMENT,
-                                       caption VARCHAR(600),
-                                       anonymous_user_id BIGINT,
-                                       member_id BIGINT,
-                                       login_flag TINYINT(1) NOT NULL ,
-                                       created_datetime DATETIME(6) NOT NULL,
-                                       updated_datetime DATETIME(6) NOT NULL,
-                                       primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS comment (
-                                       id BIGINT NOT NULL AUTO_INCREMENT,
-                                       content VARCHAR(600),
-                                       reply_target_comment_id BIGINT,
-                                       anonymous_user_id BIGINT,
-                                       member_id BIGINT,
-                                       article_id BIGINT NOT NULL,
-                                       login_flag TINYINT(1) NOT NULL,
-                                       created_datetime DATETIME(6) NOT NULL,
-                                       updated_datetime DATETIME(6) NOT NULL,
-                                       primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS article_like (
-                                            id BIGINT NOT NULL AUTO_INCREMENT,
-                                            ip_address VARCHAR(24),
-                                            member_id BIGINT,
-                                            article_id BIGINT NOT NULL,
-                                            login_flag TINYINT(1) NOT NULL,
-                                            created_datetime DATETIME(6) NOT NULL,
-                                            primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS photo_download_source (
-                                                     id BIGINT NOT NULL AUTO_INCREMENT,
-                                                     photo_resolution VARCHAR(18) NOT NULL,
-                                                     file_source VARCHAR(600) NOT NULL,
-                                                     file_byte_size BIGINT NOT NULL,
-                                                     download_count BIGINT NOT NULL,
-                                                     article_id BIGINT NOT NULL,
-                                                     created_datetime DATETIME(6) NOT NULL,
-                                                     updated_datetime DATETIME(6) NOT NULL,
-                                                     primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS hashtag (
-                                       id BIGINT NOT NULL AUTO_INCREMENT,
-                                       tag_name VARCHAR(26) NOT NULL,
-                                       created_datetime DATETIME(6) NOT NULL,
-                                       updated_datetime DATETIME(6) NOT NULL,
-                                       primary key (id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS article_hashtag (
-                                               id BIGINT NOT NULL AUTO_INCREMENT,
-                                               article_id BIGINT NOT NULL,
-                                               hashtag_id BIGINT NOT NULL,
-                                               created_datetime DATETIME(6) NOT NULL,
-                                               updated_datetime DATETIME(6) NOT NULL,
-                                               primary key (id)
-) engine=InnoDB;
+CREATE SEQUENCE member_sequence START WITH 11 INCREMENT BY 1;
 
 ALTER TABLE member
-    ADD CONSTRAINT UK_hh9kg6jti4n1eoiertn2k6qsc UNIQUE (nickname);
-
+    ADD CONSTRAINT uk_nickname_on_member UNIQUE (nickname);
 ALTER TABLE member
-    ADD CONSTRAINT UK_cgclm058f54tosrpko2h125op UNIQUE (email_address);
+    ADD CONSTRAINT uk_email_address_on_member UNIQUE (email_address);
+
+CREATE TABLE anonymous_user
+(
+    id               NUMERIC(19, 0) NOT NULL,
+    nickname         VARCHAR2(18)   NOT NULL,
+    password         VARCHAR2(64)   NOT NULL,
+    ip_address       VARCHAR2(20)   NOT NULL,
+    user_role        VARCHAR2(255)  NOT NULL CHECK (user_role IN ('ADMIN', 'MEMBER', 'ANONYMOUS')),
+    created_datetime TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence anonymous_user_sequence START WITH 8 INCREMENT BY 1;
+
+CREATE TABLE article
+(
+    id                NUMERIC(19, 0) NOT NULL,
+    caption           VARCHAR2(600)  NOT NULL,
+    anonymous_user_id NUMERIC(19, 0),
+    member_id         NUMERIC(19, 0),
+    login_flag        BOOLEAN        NOT NULL,
+    created_datetime  TIMESTAMP(6)   NOT NULL,
+    updated_datetime  TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence article_sequence START WITH 15 INCREMENT BY 1;
+
+CREATE TABLE article_comment
+(
+    id                      NUMERIC(19, 0) NOT NULL,
+    content                 VARCHAR2(600)  NOT NULL,
+    reply_target_comment_id NUMERIC(19, 0),
+    anonymous_user_id       NUMERIC(19, 0),
+    member_id               NUMERIC(19, 0),
+    article_id              NUMERIC(19, 0) NOT NULL,
+    login_flag              BOOLEAN        NOT NULL,
+    created_datetime        TIMESTAMP(6)   NOT NULL,
+    updated_datetime        TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence article_comment_sequence START WITH 31 INCREMENT BY 1;
+
+CREATE TABLE article_like
+(
+    id               NUMERIC(19, 0) NOT NULL,
+    ip_address       VARCHAR2(20),
+    member_id        NUMERIC(19, 0),
+    article_id       NUMERIC(19, 0) NOT NULL,
+    login_flag       BOOLEAN        NOT NULL,
+    created_datetime TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT article_like_member_id_article_id_unique UNIQUE (ip_address, member_id, article_id)
+);
+
+create sequence article_like_sequence START WITH 21 INCREMENT BY 1;
 
 ALTER TABLE article_like
-    ADD CONSTRAINT article_like_member_id_article_id_unique UNIQUE (ip_address, member_id, article_id);
+    ADD CONSTRAINT uk_ip_address_member_id_article_id UNIQUE (ip_address, member_id, article_id);
+
+CREATE TABLE photo_download_source
+(
+    id               NUMERIC(19, 0) NOT NULL,
+    photo_resolution VARCHAR2(18)   NOT NULL,
+    file_source      VARCHAR2(600)  NOT NULL UNIQUE,
+    file_byte_size   NUMERIC(19, 0) NOT NULL,
+    download_count   NUMERIC(19, 0) NOT NULL,
+    article_id       NUMERIC(19, 0) NOT NULL,
+    created_datetime TIMESTAMP(6)   NOT NULL,
+    updated_datetime TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence photo_download_source_sequence START WITH 21 INCREMENT BY 1;
 
 ALTER TABLE photo_download_source
-    ADD CONSTRAINT UK_ntge09558j807a9e33r0gvukq UNIQUE (file_source);
+    ADD CONSTRAINT uk_file_source UNIQUE (file_source);
+
+CREATE TABLE hashtag
+(
+    id               NUMERIC(19, 0) NOT NULL,
+    tag_name         VARCHAR2(26)   NOT NULL UNIQUE,
+    created_datetime TIMESTAMP(6)   NOT NULL,
+    updated_datetime TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence hashtag_sequence START WITH 15 INCREMENT BY 1;
 
 ALTER TABLE hashtag
-    ADD CONSTRAINT UK_3f3xt16e9nburcj5lolwpgnvh UNIQUE (tag_name);
+    ADD CONSTRAINT uk_tag_name UNIQUE (tag_name);
+
+CREATE TABLE article_hashtag
+(
+    id               NUMERIC(19, 0) NOT NULL,
+    article_id       NUMERIC(19, 0) NOT NULL,
+    hashtag_id       NUMERIC(19, 0) NOT NULL,
+    created_datetime TIMESTAMP(6)   NOT NULL,
+    updated_datetime TIMESTAMP(6)   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create sequence article_hashtag_sequence START WITH 15 INCREMENT BY 1;
+

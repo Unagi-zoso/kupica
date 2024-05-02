727b94c85da1cf0ce739966e829b256e98ab402f
corvo789 <corvo789@naver.com>
2024-05-02 20:31:27 +0900

    #52 Refactor HashtagValidator 예외 메시지 한글로 변경,  메소드 이름 변경
    예외 발생 시 한글이 읽기 좋을 것 같아 변경하였다.
    
    기존 ` returnContinuousLastHashIndex` 메소드의 이름을 `returnContinuousHashLastIndex` 으로 이름을 변경하였다.
    `Last` 라는 단어의 위치가 바뀐 것이다. 이 메소드의 목적은 첫 인덱스부터 시작하여 연속적인 해시 마크('#') 가 이어질 때 그 연속적인 해시 마크의 마지막 인덱스를 반환하는 메소드이다.
    `Last` 라는 단어가 `Hash` 앞에 붙는 것 보다 `Index` 앞에 있는 것이 더 이해하기 좋다 판단하여 바꾸었다.

--- kupica/src/main/java/org/nightdivers/kupica/domain/hashtag/HashtagValidator.java
+++ kupica/src/main/java/org/nightdivers/kupica/domain/hashtag/HashtagValidator.java
@@ -3,25 +3,25 @@ package org.nightdivers.kupica.domain.hashtag;
 import static org.nightdivers.kupica.domain.hashtag.Hashtag.MAX_TAG_NAME_WITH_HASH_LENGTH;
 
 public class HashtagValidator {
-    public static String validateTagName(String tagName) {
+    public static String returnValidTagName(String tagName) {
         if (tagName == null || tagName.isEmpty()) {
-            throw new IllegalArgumentException("Tag name must not be empty");
+            throw new IllegalArgumentException("태그 이름은 비어있을 수 없습니다.");
         }
 
         tagName = "#" + eraseContinuousHash(tagName);
 
         if (tagName.length() < 2) {
-            throw new IllegalArgumentException("Tag name must not be empty");
+            throw new IllegalArgumentException("태그 이름은 최소 2글자 이상이어야 합니다.");
         }
 
         if (tagName.length() > MAX_TAG_NAME_WITH_HASH_LENGTH) {
-            throw new IllegalArgumentException("Tag name must not exceed " + MAX_TAG_NAME_WITH_HASH_LENGTH + " characters");
+            throw new IllegalArgumentException("태그 이름은 " + MAX_TAG_NAME_WITH_HASH_LENGTH + " 을 넘을 수 없습니다.");
         }
 
         return tagName;
     }
 
-    private static int returnContinuousLastHashIndex(String tagName) {
+    private static int returnContinuousHashLastIndex(String tagName) {
         int continuousLastHashIndex = -1;
         for (int i = 0; i < tagName.length(); i++) {
             if (tagName.charAt(i) != '#') {
@@ -34,11 +34,11 @@ public class HashtagValidator {
     }
 
     private static String eraseContinuousHash(String tagName) {
-        int continuousLastHashIndex = returnContinuousLastHashIndex(tagName);
-        if (continuousLastHashIndex == -1) {
+        int continuousHashLastIndex = returnContinuousHashLastIndex(tagName);
+        if (continuousHashLastIndex == -1) {
             return tagName;
         }
 
-        return tagName.substring(continuousLastHashIndex + 1);
+        return tagName.substring(continuousHashLastIndex + 1);
     }
 }

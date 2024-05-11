0ccbb042877cecc6bd3117d96f1caa72892146a1
Unagi_zoso <corvo789@naver.com>
2024-05-12 03:43:19 +0900

    #51 Feat oauth, csrf, anonymous 관련 설정 추가
    세션으로 로그인을 구현하여 `csrf` 공격을 의식해 활성화하였다.
    
    익명 접근 시  `UserRole.ANONYMOUS` 를 권한으로 가진다. 이게 없어도 스프링 시큐리티에서 자체적으로 넣어주지만 직접 구현한 권한 객체를 사용하도록 하였다.
    
    각 경로마다 권한을 설정하였다.
    
    세션방식으로 구현하였기에 `logout` 에서 이를 초기화하는 부분이 있다.
    
    `CustomeOAuth2UserService` 에서 `DefaultOAuth2UserService` 를 포함하는데 이를 스프링 빈으로 주입 받는다. 이를 생성하는 빈을 `securityFilterChain` 을 빈으로  가지고 있는 `SecurityConfig` 에서  정의하면 순환참조가 발생해
    
    따로 `WebSecurityConfig` 라는 클래스를 뒀다.

--- kupica/src/main/java/org/nightdivers/kupica/config/SecurityConfig.java
+++ kupica/src/main/java/org/nightdivers/kupica/config/SecurityConfig.java
@@ -1,28 +1,71 @@
 package org.nightdivers.kupica.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static org.nightdivers.kupica.domain.member.UserRole.ANONYMOUS;
+import static org.nightdivers.kupica.domain.member.UserRole.MEMBER;
+import static org.nightdivers.kupica.domain.member.UserRole.SIGNING_UP;
 
-import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.nightdivers.kupica.handler.OAuth2AuthenticationSuccessHandler;
+import org.nightdivers.kupica.service.CustomOAuth2UserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .anonymous(anonymous -> anonymous
+                        .authorities(ANONYMOUS.getDescription())
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .loginPage("/login")
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                )
                 .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/oauth2/**", "/login/**").hasAnyAuthority(ANONYMOUS.getDescription(), SIGNING_UP.getDescription())
+                        .requestMatchers("/register/**", "/api/v1/auth/register").hasAuthority(SIGNING_UP.getDescription())
+                        .requestMatchers("/logout").hasAuthority(MEMBER.getDescription())
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .anyRequest().permitAll()
                 )
-                .formLogin(withDefaults())
-                .logout(withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
                 .build();
     }
 }
 
+@Configuration
+class WebSecurityConfig {
+
+    @Bean
+    public DefaultOAuth2UserService defaultOAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+}

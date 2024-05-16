5dc7ffd79f1cfa20d0bc28a1e2b2cd314f902cf5
unagi-zoso <unagi.zoso@gmail.com>
2024-05-17 00:46:17 +0900

    #50 Feat MemberController 클래스 및 테스트 코드 추가 (회원탈퇴 로직 처리)
    로그인 성공시 로그인 상태의 세션을 방출한다. `Servlet` 같은 외부 기술과 가까운 성향이 있어 `Controller` 에서 이 부분을 담당하게 하였다.

---
+++ kupica/src/main/java/org/nightdivers/kupica/controller/MemberController.java
@@ -0,0 +1,33 @@
+package org.nightdivers.kupica.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.nightdivers.kupica.service.WithdrawService;
+import org.nightdivers.kupica.service.dto.CustomOAuth2User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+@RestController
+public class MemberController {
+
+    private final WithdrawService withdrawService;
+
+    @DeleteMapping("/withdraw")
+    public ResponseEntity<Void> withdraw(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        CustomOAuth2User member = (CustomOAuth2User) authentication.getPrincipal();
+        withdrawService.withdrawUser(member.getEmail(), member.getUserRole());
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return ResponseEntity.ok().build();
+    }
+}

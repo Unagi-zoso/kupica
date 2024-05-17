ef6935ae1940aaabc66a5092b775b7c4859a9f7c
unagi-zoso <unagi.zoso@gmail.com>
2024-05-18 03:00:03 +0900

    #63 Test ArticleHashtagRepo test 테스트 데이터 생성 로직 추가
    `data-h2.sql` 의 테스트 데이터를 제거하여 직접 테스트 데이터를 생성한다.

--- kupica/src/test/java/org/nightdivers/kupica/domain/articlehashtag/ArticleHashtagRepositoryTest.java
+++ kupica/src/test/java/org/nightdivers/kupica/domain/articlehashtag/ArticleHashtagRepositoryTest.java
@@ -6,16 +6,59 @@ import static org.nightdivers.kupica.support.constant.HashtagConstant.TEST_VALID
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.nightdivers.kupica.domain.anonymoususer.AnonymousUser;
+import org.nightdivers.kupica.domain.anonymoususer.AnonymousUserRepository;
+import org.nightdivers.kupica.domain.article.Article;
+import org.nightdivers.kupica.domain.article.ArticleRepository;
+import org.nightdivers.kupica.domain.hashtag.Hashtag;
+import org.nightdivers.kupica.domain.hashtag.HashtagRepository;
 import org.nightdivers.kupica.support.annotation.RepositoryTest;
+import org.nightdivers.kupica.support.factory.AnonymousUserFactory;
+import org.nightdivers.kupica.support.factory.ArticleFactory;
 import org.springframework.data.domain.PageRequest;
 
 @RequiredArgsConstructor
 @RepositoryTest
 class ArticleHashtagRepositoryTest {
 
+    private final ArticleRepository articleRepository;
+
+    private final AnonymousUserRepository anonymousUserRepository;
+
+    private final HashtagRepository hashtagRepository;
+
     private final ArticleHashtagRepository articleHashtagRepository;
+
+    AnonymousUser givenAnonymousUser;
+
+    List<Article> givenArticles;
+
+    Hashtag givenHashtag;
+
+    List<ArticleHashtag> givenArticleHashtags;
+
+    @BeforeEach
+    void setUp() {
+        givenAnonymousUser = anonymousUserRepository.save(AnonymousUserFactory.createTestAnonymousUser1());
+        givenArticles = articleRepository.saveAll(
+                List.of(
+                        ArticleFactory.createTestAnonymousArticle1(givenAnonymousUser),
+                        ArticleFactory.createTestAnonymousArticle1(givenAnonymousUser),
+                        ArticleFactory.createTestAnonymousArticle1(givenAnonymousUser)
+                )
+        );
+        givenHashtag = hashtagRepository.save(Hashtag.of(TEST_VALID_TAG_NAME));
+        givenArticleHashtags = articleHashtagRepository.saveAll(
+                List.of(
+                        ArticleHashtag.of(givenArticles.get(0), givenHashtag),
+                        ArticleHashtag.of(givenArticles.get(1), givenHashtag),
+                        ArticleHashtag.of(givenArticles.get(2), givenHashtag)
+                )
+        );
+    }
 
     /* TARGET : ArticleHashtag 조회 테스트 */
     @DisplayName("Hashtag 와 일치하는 Article 페이지로 조회 - [성공]")
